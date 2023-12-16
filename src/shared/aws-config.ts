e8e4d258c7660bb0eded77d152cc0c14bd255ed3
Sundara saravanan <sundarasaravanan@kcubeconsulting.com>
2023-12-16 18:43:31 +0530

    issue in sync

--- src/shared/aws-config.ts
+++ src/shared/aws-config.ts
@@ -6,7 +6,7 @@ import { DBCONNECT } from '../shared/database';
 import { Credentials } from 'aws-sdk';
 
 
-    const updateAWSConfig = async () => {
+ const updateAWSConfig = async () => {
 
         
         try{
@@ -16,15 +16,20 @@ import { Credentials } from 'aws-sdk';
         const secret_access_key_query = await DBCONNECT('select access_key from public.configuration where id=1');
        const secret_access_key:string = secret_access_key_query.rows[0].access_key;
 
-       const region_query = await DBCONNECT('select region from public.configuration where id=1');
-       const region:string = secret_access_key_query.rows[0].region;
-
+    //    const region_query = await DBCONNECT('select region from public.configuration where id=1');
+    //    const region:string = region_query.rows[0].region;
+    //    console.log('keyvalue',region);
         // console.log('keyvalue',aws_key_id, secret_access_key);
+
+        const region = 'us-east-1';
         AWS.config.update({
             accessKeyId: aws_key_id,
             secretAccessKey: secret_access_key,
             region: region,
+            
         });
+
+        console.log('keyvalue1',region)
         
         // await testAWSAuthentication();
 
@@ -37,8 +42,26 @@ import { Credentials } from 'aws-sdk';
 };
 
 
-      updateAWSConfig();
-// })();
-// // Create an EC2 and RDS client
-export const ec2 = new AWS.EC2();
-export const rds = new AWS.RDS();
+//       updateAWSConfig();
+// // })();
+// // // Create an EC2 and RDS client
+
+const initializeAWS = async () => {
+    try {
+      await updateAWSConfig();
+      // Create AWS service instances after updating the configuration
+      const ec2 = new AWS.EC2();
+      const rds = new AWS.RDS();
+      
+      // Export the 'ec2' and 'rds' instances
+      return { ec2, rds };
+    } catch (error:any) {
+      console.error('Error initializing AWS:', error.message);
+      // Handle initialization error
+      throw error; // Rethrow the error if needed
+    }
+  };
+  
+  // Call the initialization function and export the instances
+  const { ec2, rds } = await initializeAWS();
+  export { ec2, rds };
